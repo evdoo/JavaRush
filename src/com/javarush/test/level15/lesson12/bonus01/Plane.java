977be854ef958019d399ea085e0886069227903e
evdoo <o.evdokima@gmail.com>
2014-10-14 19:22:56 +0400

    Осваивание статического блока

---
+++ src/com/javarush/test/level15/lesson12/bonus01/Plane.java
@@ -0,0 +1,16 @@
+package com.javarush.test.level15.lesson12.bonus01;
+
+/**
+ * Created by Olga on 14.10.2014.
+ */
+public class Plane implements Flyable {
+
+    private int passengersNumber;
+
+    public Plane(int passengers) {
+        this.passengersNumber = passengers;
+    }
+
+    public void fly() {
+    }
+}
