416ad3ef0d5b02b59d5e6ad2f85b5d4a74d5ee27
evdoo <o.evdokima@gmail.com>
2014-10-14 19:38:15 +0400

    Template pattern

---
+++ src/com/javarush/test/level15/lesson12/bonus02/DrinkMaker.java
@@ -0,0 +1,19 @@
+package com.javarush.test.level15.lesson12.bonus02;
+
+/**
+ * Created by Olga on 14.10.2014.
+ */
+public abstract class DrinkMaker {
+
+    public abstract void getRightCup();
+
+    public abstract void putIngredient();
+
+    public abstract void pour();
+
+    public void makeDrink() {
+        getRightCup();
+        putIngredient();
+        pour();
+    }
+}
